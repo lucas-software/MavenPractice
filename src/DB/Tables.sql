d01f43b0e2ddc478696421e6ddd7a34a1518fceb
lucas-software <160547725+lucas-software@users.noreply.github.com>
2024-09-29 23:07:51 -0300

    Project with maven

---
+++ src/DB/Tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO coach (id,name,age) VALUES (01,'Lucas Ribeiro', 21);
+INSERT INTO coodinator (id,role,name,age) VALUES (02,'Lucas Ribeiro','Beetlehuice', 22);
+INSERT INTO intern (id,internshipDuration) VALUES (03,18);
+INSERT INTO nba (id,numberOfTeams,sport) VALUES (81,32,'Basketball');
+INSERT INTO nfl (id,numberOfTeams,sport) VALUES (82,32,'Football');
+INSERT INTO nbateam (id,name) VALUES (91,'San Francisco Calonje');
+INSERT INTO nflteam (id,name) VALUES (01,'San Lucas Ribeiro');
+INSERT INTO player (number, nationality, position) VALUES (11,'Brazillian','Winger');
+
+
